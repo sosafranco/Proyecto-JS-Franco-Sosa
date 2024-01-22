99173c74657e6c093092523f861ab48e87e45cca
Franco Sosa <fm.sosa01@gmail.com>
2024-01-21 16:06:10 -0800

    Hola Mundo

---
+++ js/main.js
@@ -0,0 +1,16 @@
+//declarar variables
+
+let nombre;
+let apellido;
+let edad;
+
+//Asignar un valor
+
+nombre="Franco"; //string o cadena de caracteres
+apellido="Sosa";
+
+edad=23;
+
+//reasignacion
+
+edad=30
